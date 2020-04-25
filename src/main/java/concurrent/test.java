31057e7cab9768de4194ae13307bece71871a160
syl9798@163.com <syl9798@163.com>
2020-04-25 20:05:26 +0800

    Fix：守护线程

--- src/main/java/concurrent/test.java
+++ src/main/java/concurrent/test.java
@@ -1,4 +1,42 @@
 package concurrent;
 
 public class test {
+    public static void main(String[] args) {
+//        Father father =  new Sun();
+//        father.run();
+//        father.walk();
+//        ((Sun) father).hhh();
+//        System.out.println(father.s);
+        Sun s = new Sun();
+        s.run();
+    }
+}
+class Father{
+
+    public String s = "abc";
+
+    public static void fly(){
+        System.out.println("father fly");
+    }
+
+    public void run(){
+        System.out.println("father run");
+    }
+
+    public void walk(){
+        System.out.println("father walk");
+    }
+}
+class Sun extends Father{
+
+    public String s = "aa";
+
+
+    public static void fly(){
+        System.out.println("sun fly");
+    }
+
+    public void hhh(){
+        System.out.println("sun hhh");
+    }
 }
